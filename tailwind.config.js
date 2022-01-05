512304cfcef50ffee98b40a4b4fa7ef38cacc1c7
Halit Oskan <halit.oskan@and.digital>
2022-01-05 16:11:36 +0000

    init next app and tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,t$,jsx,tsx}",
+  ],
+  darkMode: "media",
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
